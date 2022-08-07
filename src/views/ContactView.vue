c7fba6a20d1b8f078557736ac221dfc5a3260fee
MasindeOdera <masindeodera@MasindeOderas-MacBook-Pro.local>
2022-08-07 17:13:42 +0200

    Updated Contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -11,8 +11,8 @@
         <tr>
             <td>Catering</td>
             <td>Chillfood</td>
-            <td>jerry.krebs@chillfood.ch</td>
-            <td>https://www.chillfood.ch/</td>
+            <td><a href="sandra.kleisa@chillfood.ch">sandra.kleisa@chillfood.ch</a></td>
+            <td><a href="https://www.chillfood.ch/">https://www.chillfood.ch/</a></td>
         </tr>
         <tr>
             <td>Wedding Cakes</td>
@@ -24,42 +24,42 @@
             <td>Drink delivery</td>
             <td>Portner Getränke</td>
             <td>+41318080010</td>
-            <td>https://portnerag.ch/</td>
+            <td><a href="https://portnerag.ch/">https://portnerag.ch/</a></td>
         </tr>
         <tr>
             <td>Photographer</td>
             <td>Andrea Rufener</td>
-            <td>hello@andrearufener.com </td>
-            <td>https://andrearufener.com/</td>
+            <td><a href="hello@andrearufener.com">hello@andrearufener.com</a></td>
+            <td><a href="https://andrearufener.com/">https://andrearufener.com/</a></td>
         </tr>
         <tr>
             <td>DJ</td>
             <td>Yvan</td>
-            <td>yvan@swissdj.ch / +41791998733</td>
-            <td>https://www.swissdj.ch/dj-yvan/</td>
+            <td><a href="yvan@swissdj.ch">yvan@swissdj.ch</a> / +41791998733</td>
+            <td><a href="https://www.swissdj.ch/dj-yvan/">https://www.swissdj.ch/dj-yvan/</a></td>
         </tr>
         <tr>
             <td>Zimt&Zucker</td>
             <td>Zimt & Zucker</td>
-            <td>info@zimt-und-zucker.ch</td>
-            <td>https://zimt-und-zucker.ch/</td>
+            <td><a href="info@zimt-und-zucker.ch">info@zimt-und-zucker.ch</a></td>
+            <td><a href="https://zimt-und-zucker.ch/">https://zimt-und-zucker.ch/</a></td>
         </tr>
         <tr>
             <td>Blütenreich</td>
             <td>Andrea Moya</td>
-            <td>Andrea.Moya@thun.ch / 41795071573</td>
-            <td>https://www.bluetenreich.ch/de/</td>
+            <td><a href="Andrea.Moya@thun.ch">Andrea.Moya@thun.ch</a> / 41795071573</td>
+            <td><a href="https://www.bluetenreich.ch/de/">https://www.bluetenreich.ch/de/</a></td>
         </tr>
         <tr>
             <td>Make up & Hair</td>
             <td>Martina Krieg</td>
-            <td>info@hochzeitsfrisur-makeup.ch</td>
-            <td>http://www.artefrisura.ch/</td>
+            <td><a href="info@hochzeitsfrisur-makeup.ch">info@hochzeitsfrisur-makeup.ch</a></td>
+            <td><a href="http://www.artefrisura.ch/">http://www.artefrisura.ch/</a></td>
         </tr>
         <tr>
             <td>Pfarrer</td>
             <td>Simon Bärtschi</td>
-            <td>sbaertschi@kirchgemeindehilterfingen.ch</td>
+            <td><a href="sbaertschi@kirchgemeindehilterfingen.ch">sbaertschi@kirchgemeindehilterfingen.ch</a></td>
             <td></td>
         </tr>
   </table>
@@ -68,6 +68,12 @@
 
 <style lang="scss" scoped>
 .contact {
+    overflow-x: auto;
+    padding: 20px;
+    h1 {
+        display: block;
+        margin: 0 auto;
+    }
     table {
         border-collapse: collapse;
         width: 70%;
@@ -75,6 +81,10 @@
 
         tr:nth-child(even) {
             background-color: #dddddd;
+        }
+
+        th {
+            cursor: default;
         }
 
         td, th {
