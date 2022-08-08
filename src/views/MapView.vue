b126626a056c06712dade62eca41c9bc69af3e80
MasindeOdera <masindeodera@MasindeOderas-MacBook-Pro.local>
2022-08-08 21:44:35 +0200

    Updated maps

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="map">
-    <h1>Imagine a Map here to direct people</h1>
+    <h1>Schloss Hünegg</h1>
+    <div style="width: 80%; display: initial"><iframe width="80%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Staatsstrasse%2052,%203652%20Hilterfingen+(Schloss%20H%C3%BCnegg)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">bike gps</a></iframe></div>
+    <h1>Kirche Hilterfingen</h1>
+    <div style="width: 100%"><iframe width="80%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Schoren%2040,%203653%20Oberhofen%20am%20Thunersee+(Kirche%20Hilterfingen)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">bike gps</a></iframe></div>
   </div>
 </template>
